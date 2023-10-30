ddf61d9776d96bff1c19952d8ed38cdd478d0775
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:21:38 +0200

    squash!

--- Source/timestamp.tsx
+++ Source/timestamp.tsx
@@ -2,45 +2,45 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import * as React from 'react';
+import * as React from "react";
 
 export const Timestamp: React.FC<{ value: string | number }> = ({ value }) => (
-  <span>{formatTimestamp(value)}</span>
+	<span>{formatTimestamp(value)}</span>
 );
 
-export const TimestampSinceEpoch: React.FC<{ value: string | number; epoch: string | number }> = ({
-  value,
-  epoch,
-}) => {
-  const [showInterval, setShowInterval] = React.useState(true);
-  const swap = React.useCallback(() => setShowInterval(!showInterval), [
-    showInterval,
-    setShowInterval,
-  ]);
+export const TimestampSinceEpoch: React.FC<{
+	value: string | number;
+	epoch: string | number;
+}> = ({ value, epoch }) => {
+	const [showInterval, setShowInterval] = React.useState(true);
+	const swap = React.useCallback(
+		() => setShowInterval(!showInterval),
+		[showInterval, setShowInterval]
+	);
 
-  const delta = new Date(value).getTime() - new Date(epoch).getTime();
-  return (
-    <span onClick={swap} role="button">
-      {showInterval ? formatInterval(delta) : formatTimestamp(value)}
-    </span>
-  );
+	const delta = new Date(value).getTime() - new Date(epoch).getTime();
+	return (
+		<span onClick={swap} role="button">
+			{showInterval ? formatInterval(delta) : formatTimestamp(value)}
+		</span>
+	);
 };
 
 const formatTimestamp = (timestamp: string | number) => {
-  return new Date(timestamp).toISOString();
+	return new Date(timestamp).toISOString();
 };
 
 const formatInterval = (totalMilliseconds: number): string => {
-  let sign = '+';
-  if (totalMilliseconds < 0) {
-    totalMilliseconds = -totalMilliseconds;
-    sign = '-';
-  }
+	let sign = "+";
+	if (totalMilliseconds < 0) {
+		totalMilliseconds = -totalMilliseconds;
+		sign = "-";
+	}
 
-  const milliseconds = (totalMilliseconds % 1000).toString().padStart(3, '0');
-  const totalSeconds = Math.floor(totalMilliseconds / 1000);
-  const seconds = (totalSeconds % 60).toString().padStart(2, '0');
-  const totalMinutes = Math.floor(totalSeconds / 60);
+	const milliseconds = (totalMilliseconds % 1000).toString().padStart(3, "0");
+	const totalSeconds = Math.floor(totalMilliseconds / 1000);
+	const seconds = (totalSeconds % 60).toString().padStart(2, "0");
+	const totalMinutes = Math.floor(totalSeconds / 60);
 
-  return `${sign}${totalMinutes}:${seconds}.${milliseconds}`;
+	return `${sign}${totalMinutes}:${seconds}.${milliseconds}`;
 };
