d47a41cdcd55ef7511804ecdf45db921431d82aa
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:36:26 +0200

    squash!

--- Source/file-upload.tsx
+++ Source/file-upload.tsx
@@ -2,37 +2,42 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import * as React from 'react';
-import { inflate } from 'pako';
+import * as React from "react";
+import { inflate } from "pako";
 
-export const FileUpload: React.FC<{ onChange(text: string): void }> = props => {
-  const uploadFile = React.useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (typeof reader.result === 'string') {
-        props.onChange(reader.result);
-        return;
-      }
+export const FileUpload: React.FC<{ onChange(text: string): void }> = (
+	props
+) => {
+	const uploadFile = React.useCallback(
+		(evt: React.ChangeEvent<HTMLInputElement>) => {
+			const reader = new FileReader();
+			reader.onload = () => {
+				if (typeof reader.result === "string") {
+					props.onChange(reader.result);
+					return;
+				}
 
-      const result = new Uint8Array(reader.result as ArrayBuffer);
-      try {
-        props.onChange(inflate(result, { to: 'string' }));
-      } catch (e) {
-        alert(`Error decompressing file: ${e}`);
-      }
-    };
+				const result = new Uint8Array(reader.result as ArrayBuffer);
+				try {
+					props.onChange(inflate(result, { to: "string" }));
+				} catch (e) {
+					alert(`Error decompressing file: ${e}`);
+				}
+			};
 
-    const file = evt.target.files?.[0];
-    if (!file) {
-      return;
-    }
+			const file = evt.target.files?.[0];
+			if (!file) {
+				return;
+			}
 
-    if (file.name.endsWith('.json') || file.name.endsWith('.txt')) {
-      reader.readAsText(file);
-    } else {
-      reader.readAsArrayBuffer(file);
-    }
-  }, []);
+			if (file.name.endsWith(".json") || file.name.endsWith(".txt")) {
+				reader.readAsText(file);
+			} else {
+				reader.readAsArrayBuffer(file);
+			}
+		},
+		[]
+	);
 
-  return <input type="file" onChange={uploadFile} />;
+	return <input type="file" onChange={uploadFile} />;
 };
